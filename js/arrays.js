b591d19b7a2ea28c300877df21858594fc3ba6bc
margonzalezz <maarlenegonzalezz@gmail.com>
2023-06-02 22:34:31 -0300

    actualizacion del proyecto con array y funciones, queda ensamblar todo

--- js/arrays.js
+++ js/arrays.js
@@ -1,14 +1,53 @@
 
-// destinos DE OBJETOS CON LOS LUGARES
+// ARRAY DE OBJETOS CON LOS DESTINOS
 const lugares = [
-    {numero: 1, lugar: "Buenos Aires, Argentina" }, 
-    {numero: 2, lugar: "New york, Estados Unidos" } ,
-    {numero: 3, lugar: "Venecia, Italia"} , 
-    {numero: 4, lugar: "Ibiza, España"} , 
-    {numero: 5, lugar: "Paris, Francia"} , 
-    {numero: 6, lugar: "Mykonos, Grecia"} , 
-    {numero: 7, lugar: "Bariloche, Argentina"} , 
-    {numero: 8, lugar: "Cataratas de Iguazú, Argentina"}];
+    {numero: 1, lugar: "Buenos Aires, Argentina" },         //Iberia - AmAirlines - Aerolineas Argentinas 
+    {numero: 2, lugar: "New york, Estados Unidos" } ,       //Iberia - AmAirlines - Aerolíneas Argentinas
+    {numero: 3, lugar: "Venecia, Italia"} ,                 //Iberia - AmAirlines - Aerolíneas Argentinas 
+    {numero: 4, lugar: "Ibiza, España"} ,                   //Iberia - Aerolíneas Argentinas 
+    {numero: 5, lugar: "Paris, Francia"} ,                  //Iberia - AmAirlines
+    {numero: 6, lugar: "Mykonos, Grecia"} ,                 //Iberia - AmAirlines 
+    {numero: 7, lugar: "Bariloche, Argentina"} ,            //Iberia - AmAirlines - Aerolíneas Argentinas
+    {numero: 8, lugar: "Cataratas de Iguazú, Argentina"}];  //Iberia - AmAirlines - Aerolíneas Argentinas
+
+    // ARRAY DE EJEMPLO DEL AFTER
+    const prendas = [
+    {imagen: '👚', codigo: 1, tipo: 'Remera rosa', precio: 2599},
+    {imagen: '🩳', codigo: 2, tipo: 'Bermuda playera', precio: 3859},
+    {imagen: '👕', codigo: 3, tipo: 'Remera casual', precio: 7851},
+    {imagen: '👖', codigo: 4, tipo: 'Pantalón de jean', precio: 8599},]
+
+    // ARRAY DE OBJETOS CON LAS AEROLÍNEAS DISPONIBLES
+    const aerolíneas = [
+        {aerolínea1: "Aerolíneas Argentinas", precio: 250000} ,
+        {aerolínea2: "Iberia", precio: 280000} ,
+        {aerolínea3: "American Airlines", precio: 305000}]
+
+    const opcionesPasajes = [
+        {nroPasaje: 1, origen: origenSeleccionado.lugar, destino: destinoSeleccionado.lugar}
+    ]
+
+    //EJECUTAR EL MÉTODO filter PARA QUE BUSQUE DENTRO DEL ARRAY SELECCIONADO Y ME DEVUELVA
+    //LAS COINCIDENCIAS DE LAS AEROLÍNEAS Y LUEGO, USAR EL MISMO ARRAY PARA LOS PRECIOS
+
+    //PARA EL PASAJE VOY A NECESITAR: origen - destino - aerolínea - precio
+
+
+
+    /*
+    const opcionesViaje = [
+        { aerolinea: 'Aerolínea A', precio: 200 },
+        { aerolinea: 'Aerolínea B', precio: 150 },
+        { aerolinea: 'Aerolínea C', precio: 300 }
+      ];
+      
+      // Acceder a las opciones de viaje
+      for (let i = 0; i < opcionesViaje.length; i++) {
+        console.log(opcionesViaje[i].aerolinea + ' - Precio: $' + opcionesViaje[i].precio);
+      }
+    */
+
+
 
 
 
--- js/main.js
+++ js/main.js
@@ -2,69 +2,93 @@
   let nombre = prompt("Por favor, ingrese su nombre");
   let selectIdaYvuelta;
 
-  // FUNCIÓN PARA CUANDO EL USUARIO ELIGE SÓLO LA OPCIÓN IDA
-  let fechaIda;
-  let fechaIngrIda;
-  let fechaActual;
-  function soloIda() {
-    fechaIda = prompt("Ingrese la fecha de partida con el formato (dd/mm/aaaa): ");
-    fechaIngrIda = new Date(fechaIda);
-    fechaActual = new Date();
-        while (fechaIngrIda < fechaActual) {
-          alert("Por favor, ingrese una fecha válida.");
-          fechaIda;
-        }
-  }
+  //NOMBRE DEL USUARIO QUE REALIZA LA RESERVA
+          // FUNCIÓN PARA CUANDO EL USUARIO SÓLO QUIERE VIAJAR DE IDA
+          let fechaIda;
+          let fechaIngrIda;
+          let fechaActual;
 
+          function soloIda() {
+            fechaIda = prompt("Ingrese la fecha de partida con el formato (dd/mm/aaaa): ");
+            fechaIngrIda = new Date(fechaIda);
+            fechaActual = new Date();
+            while (fechaIngrIda < fechaActual) {
+              alert("Por favor, ingrese una fecha válida");
+            }
+          }
+          // FUNCIÓN PARA CUANDO EL USUARIO QUIERE REGRESAR
+          let fechaRegreso;
+          let fechaIngRegreso;
+
+          function regreso() {
+            fechaRegreso = prompt("Ingrese la fecha de regreso con el formato (dd/mm/aaaa): ");
+            fechaIngRegreso = new Date(fechaRegreso);
+            fechaActual = new Date();
+              while (fechaIngRegreso < fechaActual) {
+                alert("Por favor, ingrese una fecha válida");
+              }
+          }
+
+  // FUNCION PARA PREGUNTARLE AL USUARIO EL TIPO DE PASAJE QUE DESEA SELECCIONAR
   function select() {
     let selectIda = confirm("Hola, " + nombre + " ¿Deseas reservar un asiento sólo de ida?" + "\nSi es así, presiona Aceptar para continuar")
       if (selectIda == true) {
         alert("A continuación, selecciona las opciones que más se adapten a tu búsqueda");
         soloIda();
       } else {
-          selectIdaYvuelta = confirm("¿Deseas reservar un asiento de ida y vuelta?")
+          selectIdaYvuelta = confirm("¿Deseas reservar un asiento de ida y vuelta?" + "\nSi es así, presiona Aceptar para continuar")
           if (selectIdaYvuelta == true) {
             alert("A continuación, selecciona las opciones que más se adapten a tu búsqueda");
+            soloIda();
+            regreso();
           } else{
             alert("No dudes en regresar cuando decidas tu viaje :)")
+            return
           }
       }
   }
 select()
 
-
-  // MOSTRARLE AL USUARIO LOS DESTINOS QUE TIENE DISPONIBLES
-  function lugaresDisponibles(destinos) {
-    for (let i = 0; i < destinos.length; i++) {
-      console.log(destinos[i].numero + ". " + destinos[i].lugar);
+  //FUNCIÓN PARA QUE EL USUARIO ELIJA ENTRE ALGUNOS DE LOS SIGUIENTES ORIGENES PREDETERMINADOS
+  function selectOrigen(origenes) {
+    for (let i = 0; i < origenes.length; i++) {
+      console.log(origenes[i].numero + ". " + origenes[i].lugar);
     }
   }
-  // LLAMO A LA FUNCIÓN PARA MOSTRAR LOS LUGARES
-  lugaresDisponibles(lugares);
-
-  
+  selectOrigen(lugares);
+  // SELECCIONAR EL ORIGEN
+  let origen = parseInt(prompt("Por favor, ingrese el número asignado a su origen"));
+    if (!isNaN(origen)) {
+      let origenSeleccionado = lugares.find(lugar => lugar.numero === origen);
+        if (origenSeleccionado) {
+          alert("Usted quiere viajar desde: " + origenSeleccionado.lugar);      //origenSeleccionado.lugar ES LO QUE VOY A USAR PARA CONSTRUIR EL PASAJE
+        } else {
+          alert("El origen seleccionado no es válido");
+        }
+    } else {
+      console.log("El valor ingresado no es válido.");
+    }
   // SELECCION DE UN DESTINO
-  const destino = parseInt(prompt("Por favor, ingrese el número asignado al destino al que desea viajar"));
+  let destino = parseInt(prompt("Por favor, ingrese el número asignado al destino al que desea viajar"));
     if (!isNaN(destino)) {
-      const destinoSeleccionado = lugares.find(lugar => lugar.numero === destino);
+      let destinoSeleccionado = lugares.find(lugar => lugar.numero === destino);
         if (destinoSeleccionado) {
-          alert("Usted ha seleccionado: " + destinoSeleccionado.lugar);
-          // Puedes realizar otras acciones relacionadas con el destino seleccionado
+          alert("Usted quiere viajar a: " + destinoSeleccionado.lugar);     // destinoSeleccionado.lugar ES LO QUE VOY A USAR PARA CONSTRUIR EL PASAJE     
         } else {
           alert("El destino seleccionado no está disponible.");
         }
     } else {
       console.log("El valor ingresado no es válido.");
     }
+  // CANTIDAD DE PASAJEROS A VIAJAR
+      let cantPasajeros = isNaN(prompt("Por favor, indiquenos la cantidad de pasajeros a viajar"));     //LA CANTIDAD DE PASAJEROS SELECCIONADA LA TENGO QUE MULTIPLICAR POR EL IMPORTE MÁS ADELANTE
 
-
-
-  // FUNCION PARA LA CANTIDAD DE PASAJEROS
-
-    // VARIABLE GLOBAL
-      const cantPasajeros = isNaN(prompt("Por favor, indiquenos la cantidad de pasajeros a viajar"))
-      alert(cantPasajeros)
-    // function pasajeros() {
-
+  //FUNCIÓN PARA CALCULAR UN IMPUESTO PAÍS 
+  const impuesto = dhfd
+  function impuestoPaís() {
+    if (origenSeleccionado.numero === 1 || origenSeleccionado.numero === 7 || origenSeleccionado.numero === 8) {
       
-    // }
+    }
+  }
+
+
