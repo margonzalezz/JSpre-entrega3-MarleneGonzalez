934f6bc121bada8d6ee3193c694d324d274dfb8f
margonzalezz <maarlenegonzalezz@gmail.com>
2023-06-11 13:57:38 -0300

    actualizacion de los botones del form para crear pasajes

--- js/index.js
+++ js/index.js
@@ -1,40 +1,77 @@
 // CONECCIÓN A LOS BOTONES PARA SELECCIONAR EL TIPO DE PASAJE A ELEGIR
 const botonIda = document.getElementById("botonIda");
 const botonIdaVuelta = document.getElementById("botonIdaVuelta");
+let tipoPasaje = "";   // Variable para almacenar el resultado de los botones
 
-let tipoPasaje = "";            //VARIABLE PARA ALMACENAR EL RESULTADO DE LOS BOTONES
+function obtenerTipoPasaje() {      //función obtenerTipoPasaje para almacenar el valor que elija el usuario ida o ida y vuelta
+  function botonTipoIda() {
+    botonIda.addEventListener("click", (e) => {tipoPasaje = "solo_ida";mostrarResultado()});
+  } 
+  botonTipoIda();
 
-function botonTipoIda() {
-    botonIda.addEventListener("click", (e) => {
-        tipoPasaje = "solo_ida";
-        alert("Usted quiere un pasaje: " + tipoPasaje);
-    })
+  function botonTipoIyV() {
+    botonIdaVuelta.addEventListener("click", (e) => {tipoPasaje = "ida_y_vuelta";mostrarResultado()});
+  } 
+  botonTipoIyV();
+
+  function mostrarResultado() {
+    const resultadoFinal = { tipoPasaje: tipoPasaje };
+    alert("La opción seleccionada es: " + resultadoFinal.tipoPasaje);
+  }
 }
-botonTipoIda();
-function botonTipoIyV() {
-    botonIdaVuelta.addEventListener("click", (e) =>{
-        tipoPasaje = "ida_y_vuelta";
-        alert("Usted quiere un pasaje:" + tipoPasaje);
-    })
-}
-botonTipoIyV();
+obtenerTipoPasaje();
 
 // CONECCIÓN PARA SELECCIONAR EL ORIGEN
-
 const seleccionOrigen = document.getElementById("seleccionOrigen");
 const seleccionDestino = document.getElementById("seleccionDestino");
-const selectedOrigin = "";
+let selectedOrigin = "";
+let selectedDestiny = "";
 
 function changeOrigen() {
     seleccionOrigen.addEventListener("change", (e) => {
         selectedOrigin = e.target.value;
-        alert("Opción seleccionada:", selectedOption);
+        alert("Opción seleccionada:" + selectedOrigin);
     })
 }
-
 changeOrigen();
+function changeDestino() {
+    seleccionDestino.addEventListener("change", (e) => {
+        selectedDestiny = e.target.value;
+        alert("El destino seleccionado es: " + selectedDestiny)
+    })
+}
+changeDestino();
 
+//CONECCIÓN PARA LA CANTIDAD DE PASAJEROS
+const inputPasajeros = document.getElementById("inputPasajeros");
+let valorPasajeros = "";
 
+function cantPasajeros() {
+    inputPasajeros.addEventListener("input", (e) => {
+        valorPasajeros = e.target.value;
+        alert("La cantidad de pasajeros seleccionada es: " + valorPasajeros)
+    })
+}
+cantPasajeros();
 
+//CONECCIÓN PARA LAS FECHAS DE PARTIDA Y DE REGRESO
+const inputPartida = document.getElementById("inputPartida");
+const inputRegreso = document.getElementById("inputRegreso");
+let valorFechaPartida = "";
+let valorFechaRegreso = "";
 
+function obtenerFechaPartida() {
+    inputPartida.addEventListener("input", (e) => {
+        valorFechaPartida = e.target.value;
+        alert("La fecha seleccionada es: " + valorFechaPartida);
+    })
+}
+obtenerFechaPartida();      //LA FECHA SE ALMACENA COMO  aaaa/mm/dd = año/mes/día
 
+function obtenerFechaRegreso() {
+    inputRegreso.addEventListener("input", (e) => {
+        valorFechaRegreso = e.target.value;
+        alert("La fecha de regreso seleccionada es: " + valorFechaRegreso);
+    })
+    
+}
