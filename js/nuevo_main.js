8d0337ab204eeb34c439bc789a910fa5b02d3ce9
margonzalezz <maarlenegonzalezz@gmail.com>
2023-06-12 19:38:40 -0300

    actualizacion

--- js/nuevo_main.js
+++ js/nuevo_main.js
@@ -1,102 +1,61 @@
+//  EVENTOS
 const form = document.getElementById("containerViaje");
 let reserva;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const botonIda = document.getElementById("botonIda");
+    const botonIdaVuelta = document.getElementById("botonIdaVuelta");
+    const seleccionOrigen = document.getElementById("seleccionOrigen").value;
+    const seleccionDestino = document.getElementById("seleccionDestino").value;
+    const inputPasajeros = document.getElementById("inputPasajeros").value;
+    const inputPartida = document.getElementById("inputPartida").value;
+    const inputRegreso = document.getElementById("inputRegreso").value;
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault()
-        const botonIda = document.getElementById("botonIda");
-        const botonIdaVuelta = document.getElementById("botonIdaVuelta");
-        const seleccionOrigen = document.getElementById("seleccionOrigen").value;
-        const seleccionDestino = document.getElementById("seleccionDestino").value;
-        const inputPasajeros = document.getElementById("inputPasajeros").value;
-        const inputPartida = document.getElementById("inputPartida").value;
-        const inputRegreso = document.getElementById("inputRegreso").value;
+    if (botonIda == botonIda.checked) {
+        botonIdaVuelta.disabled = true;
+    }
 
-        if (seleccionOrigen == "" || seleccionDestino == "" || inputPasajeros == "" || inputPartida == "" || inputRegreso == "") {
-            alert("Por favor, complete todos los campos")
-        }
-        if (botonIda.checked) {
-             reserva = {
-                pasaje: botonIda.value,
-                origen: seleccionOrigen,
-                destino: seleccionDestino,
-                pasajeros: inputPasajeros,
-                partida: inputPartida
-            }
-            console.log(reserva);
-        } else if (botonIdaVuelta.checked) {
-             reserva = {
-                pasaje: botonIdaVuelta.value,
-                origen: seleccionOrigen, 
-                destino: seleccionDestino,
-                pasajeros: inputPasajeros,
-                partida: inputPartida,
-                regreso: inputRegreso
-            }
-        }
-        console.log(reserva);
-    })
-/*
-//EL TIPO DE PASAJE NO REALIZA NINGUNA ACCIÓN MÁS QUE ALMACENAR EL 
-//VALOR PARA CONSTRUIR EL PASAJE
+    if (seleccionOrigen == "" || seleccionDestino == "" || inputPasajeros == "" || inputPartida == "") {
+        alert("Por favor, complete todos los campos");
+        return;
+    }
 
-// VALIDACION DE ORIGEN
-function validacionOrigen() {
-    selectedOrigin >= 1 ? true : alert("Por favor, seleccione un origen");
-}
-validacionOrigen();
-//  VALIDACION DE DESTINO
-function validacionDestino() {
-    selectedDestiny >= 1 ? true : alert("Por favor, ingrese un destino");
-}
-validacionDestino();
-//  VALIDACION DE CANTIDAD DE PASAJEROS
-function validacionPasajeros() {
-    cantPasajeros();
-    valorPasajeros >= 1 ? true : alert("Por favor, ingrese una cantidad válida");
-}
-//  VALIDACION DE FECHAS DE PARTIDA Y DE REGRESO 
-let fechaActual = new Date();
+    reserva = {
+        pasaje: botonIda.checked ? botonIda.value : botonIdaVuelta.value,
+        origen: seleccionOrigen,
+        destino: seleccionDestino,
+        pasajeros: inputPasajeros,
+        partida: inputPartida,
+        regreso: inputRegreso.value
+    };
+    console.log(reserva);
+});
 
-function selectFechaPartida() {
-    obtenerFechaPartida();
-    valorFechaPartida > fechaActual ? true : alert("Por favor, ingrese una fecha válida");
-}
+//  -------------ARRAYS--------------
+const lugares = [
+    {numero: 1, lugar: "Buenos Aires, Argentina" },         //Iberia - AmAirlines - Aerolineas Argentinas 
+    {numero: 2, lugar: "New york, Estados Unidos" } ,       //Iberia - AmAirlines - Aerolíneas Argentinas
+    {numero: 3, lugar: "Venecia, Italia"} ,                 //Iberia - AmAirlines - Aerolíneas Argentinas 
+    {numero: 4, lugar: "Ibiza, España"} ,                   //Iberia - Aerolíneas Argentinas 
+    {numero: 5, lugar: "Paris, Francia"} ,                  //Iberia - AmAirlines
+    {numero: 6, lugar: "Mykonos, Grecia"} ,                 //Iberia - AmAirlines 
+    {numero: 7, lugar: "Bariloche, Argentina"} ,            //Iberia - AmAirlines - Aerolíneas Argentinas
+    {numero: 8, lugar: "Cataratas de Iguazú, Argentina"}];  //Iberia - AmAirlines - Aerolíneas Argentinas
 
-//  FECHA DE PARTIDA: 
-function selectFechaRegreso () {
-    obtenerFechaRegreso();
-    valorFechaRegreso >= fechaActual && valorFechaRegreso > valorFechaPartida ? true : alert("Por favor, ingrese una fecha válida");
-}
-selectFechaPartida();
-selectFechaRegreso();
-*/
+    const aerolineas = [
+        {aerolinea: "Aerolíneas Argentinas", precio: 250000, habilitado:[1, 2, 3, 4, 7, 8] } ,
+        {aerolinea: "Iberia", precio: 280000, habilitado: [1, 2, 3, 4, 5, 6, 7, 8] } ,
+        {aerolinea: "American Airlines", precio: 305000, habilitado: [1, 2, 3, 5, 6, 7, 8] }];
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // FUNCIÓN PARA ENCONTRAR UNA AEROLÍNEA QUE COINCIDA CON EL NRO INDICADO EN EL DESTINO
+  let nameAir;
+  function encontrarAerolinea(destino) { // recibimos el objeto destino
+   let numeroLugar = seleccionDestino; // accedemos al número
+      const coincidencia = aerolineas.filter(aerolinea =>aerolinea.habilitado.includes(numeroLugar));
+      if(coincidencia == true) {
+        nameAir = (coincidencia.aerolinea.precio);
+      } 
+        return coincidencia;
+  }
+  const airEncontrada = encontrarAerolinea(seleccionDestino);
