7b8ebfc5db26d0be1397c1c42bb4d7741c5b23aa
margonzalezz <maarlenegonzalezz@gmail.com>
2023-06-12 22:51:26 -0300

    actualizacion de aerolineas

--- js/nuevo_main.js
+++ js/nuevo_main.js
@@ -1,36 +1,3 @@
-//  EVENTOS
-const form = document.getElementById("containerViaje");
-let reserva;
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const botonIda = document.getElementById("botonIda");
-    const botonIdaVuelta = document.getElementById("botonIdaVuelta");
-    const seleccionOrigen = document.getElementById("seleccionOrigen").value;
-    const seleccionDestino = document.getElementById("seleccionDestino").value;
-    const inputPasajeros = document.getElementById("inputPasajeros").value;
-    const inputPartida = document.getElementById("inputPartida").value;
-    const inputRegreso = document.getElementById("inputRegreso").value;
-
-    if (botonIda == botonIda.checked) {
-        botonIdaVuelta.disabled = true;
-    }
-
-    if (seleccionOrigen == "" || seleccionDestino == "" || inputPasajeros == "" || inputPartida == "") {
-        alert("Por favor, complete todos los campos");
-        return;
-    }
-
-    reserva = {
-        pasaje: botonIda.checked ? botonIda.value : botonIdaVuelta.value,
-        origen: seleccionOrigen,
-        destino: seleccionDestino,
-        pasajeros: inputPasajeros,
-        partida: inputPartida,
-        regreso: inputRegreso.value
-    };
-    console.log(reserva);
-});
-
 //  -------------ARRAYS--------------
 const lugares = [
     {numero: 1, lugar: "Buenos Aires, Argentina" },         //Iberia - AmAirlines - Aerolineas Argentinas 
@@ -47,15 +14,69 @@ const lugares = [
         {aerolinea: "Iberia", precio: 280000, habilitado: [1, 2, 3, 4, 5, 6, 7, 8] } ,
         {aerolinea: "American Airlines", precio: 305000, habilitado: [1, 2, 3, 5, 6, 7, 8] }];
 
+// ENCONTRAR AEROLÍNEA
+/*
+let nombreAerolinea;
+function encontrarAerolinea(seleccionDestino) { // recibimos el objeto destino
+ let numeroLugar = seleccionDestino.value; // accedemos al número
+    const coincidencia = aerolineas.filter(aerolinea => aerolinea.habilitado.includes(numeroLugar));
+    //coincidencia == true ? nombreAerolinea = (coincidencia.aerolinea.precio) : alert("No se encontró una aerolínea para llevarte a tu destino seleccionado")
+      //return coincidencia;
+    if(coincidencia == true) {
+      nombreAerolinea = (coincidencia.aerolinea.precio);
+    }
+      return coincidencia;
+}
+const airEncontrada = encontrarAerolinea(seleccionDestino.value); 
+*/
+// ENCONTRAR AEROLÍNEA
+let aerolineaEncontrada; // Variable para almacenar la aerolínea encontrada
 
-  // FUNCIÓN PARA ENCONTRAR UNA AEROLÍNEA QUE COINCIDA CON EL NRO INDICADO EN EL DESTINO
-  let nameAir;
-  function encontrarAerolinea(destino) { // recibimos el objeto destino
-   let numeroLugar = seleccionDestino; // accedemos al número
-      const coincidencia = aerolineas.filter(aerolinea =>aerolinea.habilitado.includes(numeroLugar));
-      if(coincidencia == true) {
-        nameAir = (coincidencia.aerolinea.precio);
-      } 
-        return coincidencia;
-  }
-  const airEncontrada = encontrarAerolinea(seleccionDestino);
+function encontrarAerolinea(seleccionDestino) {
+    let numeroLugar = seleccionDestino.value;
+    const coincidencia = aerolineas.filter(aerolinea => aerolinea.habilitado.includes(numeroLugar));
+    
+    if (coincidencia.length > 0) {
+        aerolineaEncontrada = coincidencia[0].aerolinea; // Almacenar la aerolínea encontrada
+    } else {
+        aerolineaEncontrada = ""; // Reiniciar la variable si no se encuentra aerolínea
+    }
+    
+    return coincidencia;
+}
+
+//------------EVENTOS---------------------
+document.getElementById('botonIda').addEventListener('click', (e) => {inputRegreso.disabled = true});
+document.getElementById('botonIdaVuelta').addEventListener('click', (e) => {inputRegreso.disabled = false});
+
+const form = document.getElementById("containerViaje");
+let reserva;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const botonIda = document.getElementById("botonIda");
+    const botonIdaVuelta = document.getElementById("botonIdaVuelta");
+    const seleccionOrigen = document.getElementById("seleccionOrigen").value;
+    const seleccionDestino = document.getElementById("seleccionDestino").value;
+    const inputPasajeros = document.getElementById("inputPasajeros").value;
+    const inputPartida = document.getElementById("inputPartida").value;
+    const inputRegreso = document.getElementById("inputRegreso").value;
+
+    if (seleccionOrigen == "" || seleccionDestino == "" || inputPasajeros == "" || inputPartida == "") {
+        alert("Por favor, complete todos los campos");
+        return;
+    }
+    encontrarAerolinea(seleccionDestino); // Llamar a encontrarAerolinea con el valor seleccionado
+
+    reserva = {
+        pasaje: botonIda.checked ? botonIda.value : botonIdaVuelta.value,
+        origen: seleccionOrigen,
+        destino: seleccionDestino,
+        pasajeros: inputPasajeros,
+        partida: inputPartida,
+        regreso: inputRegreso.value,
+        aerolinea: aerolineaEncontrada
+    }
+    console.log(reserva);
+});
+
+
