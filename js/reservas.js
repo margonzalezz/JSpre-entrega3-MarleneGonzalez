ce0a251cc4f6d571c83cec7efa02b0feb4e30bd2
margonzalezz <maarlenegonzalezz@gmail.com>
2023-06-17 20:56:19 -0300

    terminado, resta quitar alert

--- js/reservas.js
+++ js/reservas.js
@@ -1,15 +1,23 @@
 const reservas = JSON.parse(localStorage.getItem("misReservas"));
-// Verificar si hay reservas almacenadas
+const miPasaje = [];
 
 if (reservas && reservas.length > 0) {
-
   const contenedorVuelos = document.getElementById("containerPasajes");
+
+  const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+                        <div class="alert alert-${type} alert-dismissible divSuccess" role="alert">
+                          <div>${message}</div>
+                          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+    contenedorVuelos.insertBefore(wrapper, contenedorVuelos.firstChild);
+  };
+
   reservas.forEach(reserva => {
     const cardsPasajes = document.createElement("div");
-    // Generar el nombre de la aerolínea y la ruta de la imagen
     const nombreAerolinea = reserva.aerolinea.toLowerCase().replace(/\s/g, "-");
     const rutaImagen = `../img/icon/${nombreAerolinea}.png`;
-    // Generar el contenido dinámico para cada reserva
 
     cardsPasajes.innerHTML = `
       <div id="grid-pasajes">
@@ -18,12 +26,12 @@ if (reservas && reservas.length > 0) {
           <span class="nombreLogoAerolinea">${reserva.aerolinea}</span>
         </div>
         <div id="seccion-ida" class="ida">
-          <span class="titulo-ida">IDA</span>
+        <p class="titulo-ida">ORIGEN:<br>${reserva.origen}</p>
           <p class="fecha-ida">${reserva.partida}</p>
         </div>
         ${reserva.pasaje === "solo_ida" ? "" :
         `<div id="seccion-idaYvuelta" class="vuelta">
-            <span class="titulo-vuelta">REGRESO</span>
+          <p class="titulo-vuelta">DESTINO:<br>${reserva.destino}</p>
             <p class="fecha-vuelta">${reserva.regreso}</p>
         </div>`}
         ${reserva.pasaje === "solo_ida" ? '' :
@@ -31,45 +39,22 @@ if (reservas && reservas.length > 0) {
         <div id="container-precio" class="precio">
           <div class="precio-pasaje">
             <span class="titulo">PRECIO</span>
-            <p>${"$" + reserva.precio}</p>      
+            <p>${"$" + reserva.precio}</p>
           </div>
         </div>
-        <div class="seleccionar-pasaje" id="${reserva.id}"><button>Seleccionar</button></div>
+        <div class="seleccionar-pasaje" id="${reserva.id}">
+          <button type="button" class="btn" id="liveAlertBtn">Seleccionar</button>
+        </div>
       </div>
     `;
     contenedorVuelos.appendChild(cardsPasajes);
 
-function obtenerBotones(reserva) {
-        const botonSeleccionar = cardsPasajes.querySelector('.seleccionar-pasaje button');
-            botonSeleccionar.addEventListener('click', () => {
-        const idReserva = reserva.id;
-        const reservaSeleccionada = reservas.find(reserva => reserva.id === idReserva);
-        alert("El pasaje fue seleccionado con éxito")
-        });
-}
-obtenerBotones(reserva);
-
-    
+    function obtenerBotones(reserva) {
+      const botonSeleccionar = cardsPasajes.querySelector('.seleccionar-pasaje button');
+      botonSeleccionar.addEventListener('click', () => {
+        appendAlert('La reserva fue realizada con éxito.', 'success');
+      });
+    }
+    obtenerBotones(reserva);
   });
 }
-
-
-
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
